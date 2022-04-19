4181ee4dd4060e6f883dcf27bcc25470d5956b8b
Calos Lobo <cslobosilva@gmail.com>
2022-04-18 22:42:15 -0500

    ➕ ADD: Creación Crud users component

--- src/app/home/crud-users/crud-users.component.ts
+++ src/app/home/crud-users/crud-users.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from "@angular/core";
+import { MatDialog } from '@angular/material/dialog';
+import { PageEvent } from "@angular/material/paginator";
+import { User } from "../../interfaces/api-user.interface";
+import { ApiUsersService } from "../../services/api-users.service";
+import { DialogCreateUserComponent } from './components/dialog-create-user/dialog-create-user.component';
 
-const ELEMENT_DATA: any[] = [
-  { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
-  { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
-  { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
-  { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
-  { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
-  { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-  { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
-  { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" },
-];
+const INITIAL_PAGINATOR_VALUE = {
+  length: 0,
+  pageIndex: 0,
+  pageSize: 5,
+};
 
 @Component({
   selector: "app-crud-users",
@@ -19,10 +17,34 @@ const ELEMENT_DATA: any[] = [
   styleUrls: ["./crud-users.component.scss"],
 })
 export class CrudUsersComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  public length = 0;
+  public pageSize = 0;
 
-  constructor() {}
+  public users: User[] = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private apiUsersService: ApiUsersService,
+    public dialog: MatDialog
+  ) {}
+
+  public ngOnInit(): void {
+    this.getListUsers(INITIAL_PAGINATOR_VALUE);
+  }
+
+  public getListUsers(event: PageEvent): void {
+    this.apiUsersService.getListUsers(event).subscribe((response) => {
+      this.users = response.data;
+
+      this.pageSize = response.per_page;
+      this.length = response.total;
+    });
+  }
+
+  public openDialog() {
+    const dialogRef = this.dialog.open(DialogCreateUserComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
--- src/app/home/crud-users/crud-users.module.ts
+++ src/app/home/crud-users/crud-users.module.ts
@@ -1,13 +1,28 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { CrudUsersComponent } from "./crud-users.component";
-import { CrudUsersRoutingModule } from "./crud-users-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+
+import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatTableModule } from "@angular/material/table";
-import { ModalComponent } from './components/modal/modal.component';
-import { FormCreateUserComponent } from './components/form-create-user/form-create-user.component';
+import { CrudUsersRoutingModule } from "./crud-users-routing.module";
+import { CrudUsersComponent } from "./crud-users.component";
+import { TableUsersComponent } from "./components/table-users/table-users.component";
+import { MatDialogModule } from "@angular/material/dialog";
+import { DialogCreateUserComponent } from './components/dialog-create-user/dialog-create-user.component';
 
 @NgModule({
-  declarations: [CrudUsersComponent, ModalComponent, FormCreateUserComponent],
-  imports: [CommonModule, CrudUsersRoutingModule, MatTableModule],
+  declarations: [
+    CrudUsersComponent,
+    TableUsersComponent,
+    DialogCreateUserComponent,
+  ],
+  imports: [
+    CommonModule,
+    CrudUsersRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    HttpClientModule,
+    MatDialogModule
+  ],
 })
 export class CrudUsersModule {}
